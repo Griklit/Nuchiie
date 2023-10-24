88612452d6b730b55f0c2d7a968df965a9482d5d
Sequence[7] <sequence7@scalt.sct>
2023-10-24 15:22:56 +0800

    js、lua、sql

---
+++ src/bin/javascript.rs
@@ -0,0 +1,18 @@
+use std::fs;
+use std::io::Write;
+
+use nuchiie::ToChinese;
+
+fn main() {
+    let mut f = fs::File::create("output/arabic_to_chinese.js").expect("Create file failed.");
+    f.write("function arabicToChinese(num) {\n".as_bytes()).unwrap();
+    f.write("    if (num === 0) {\n".as_bytes()).unwrap();
+    f.write("        return \"零\";\n".as_bytes()).unwrap();
+    for i in 1u16..=u16::MAX {
+        f.write(format!("    }} else if (num === {}) {{\n", i).as_bytes()).unwrap();
+        f.write(format!("        return \"{}\";\n", i.to_chinese()).as_bytes()).unwrap();
+    }
+    f.write("    }\n".as_bytes()).unwrap();
+    f.write("    return undefined;\n".as_bytes()).unwrap();
+    f.write("}\n".as_bytes()).unwrap();
+}
