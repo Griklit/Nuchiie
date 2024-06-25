83ca8cff334f9c4486dcaba5fd3a2c611c759714
seq7 <sequence7@scalt.sct>
2024-06-25 09:06:43 +0800

    ...

--- src/bin/haskell.rs
+++ src/bin/haskell.rs
@@ -11,14 +11,14 @@ numberToChinese :: Int -> ConversionResult
 NumberToChinese number
 "#;
 
-const END: &str = r#"  | otherwise = ErrorMsg "The number is out of range."
+const END: &str = r#"    | otherwise = ErrorMsg "The number is out of range."
 "#;
 
 fn main() {
     let mut f = fs::File::create("output/NumberToChinese.hs").expect("Create file failed.");
     f.write(START.as_bytes()).unwrap();
     for i in 1u16..=u16::MAX {
-        f.write(format!("  | number == {:<5} = SuccessMsg \"{}\"\n", i, i.to_chinese()).as_bytes())
+        f.write(format!("    | number == {:<5} = SuccessMsg \"{}\"\n", i, i.to_chinese()).as_bytes())
             .unwrap();
     }
     f.write(END.as_bytes()).unwrap();
